51d2505d12899f5d8400de1e0c0669f1a3b51fa5
Raja <rajad@decipherzone.com>
2024-08-22 11:16:05 +0530

    formatting fixes

--- src/main/java/com/cleverthis/interview/Solution.java
+++ src/main/java/com/cleverthis/interview/Solution.java
@@ -16,29 +16,29 @@ public class Solution {
     private int numpadSize;
     private PadlockImpl padlock;
 
-    public void solve(PadlockImpl padlock){
+    public void solve(PadlockImpl padlock) {
         this.numpadSize = padlock.getNumpadSize();
         this.padlock = padlock;
         Integer[] solution = new Integer[numpadSize];
         Set<Integer> usedDigits = new HashSet<>();
-        if (recurse(0, solution, usedDigits)){
+        if (recurse(0, solution, usedDigits)) {
             System.out.println(Stream.of(solution).map(String::valueOf).collect(Collectors.joining()));
         }
     }
 
 
-    private boolean recurse(int position, Integer[] solution, Set<Integer> usedDigits){
-        if (position == numpadSize){
+    private boolean recurse(int position, Integer[] solution, Set<Integer> usedDigits) {
+        if (position == numpadSize) {
             return padlock.isPasscodeCorrect();
         }
 
-        for (int digit = 0; digit < numpadSize; digit++){
-            if (!usedDigits.contains(digit)){
+        for (int digit = 0; digit < numpadSize; digit++) {
+            if (!usedDigits.contains(digit)) {
                 padlock.writeInputBuffer(position, digit);
                 solution[position] = digit;
                 usedDigits.add(digit);
 
-                if(recurse(position + 1, solution, usedDigits)){
+                if (recurse(position + 1, solution, usedDigits)) {
                     return true;
                 }
                 usedDigits.remove(digit);
