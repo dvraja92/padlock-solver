705f228db916c2910aea8157a0e964f0d75938f4
Raja <rajad@decipherzone.com>
2024-08-22 17:27:10 +0530

    refactor and code comments

--- src/main/java/com/cleverthis/interview/padlock/cracker/AbstractPadlockSolution.java
+++ src/main/java/com/cleverthis/interview/padlock/cracker/AbstractPadlockSolution.java
@@ -4,8 +4,20 @@ import com.cleverthis.interview.padlock.PadlockImpl;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
+/**
+ * This abstract class defines the overall solution that uses the the backtracking process.
+ * It uses abstract methods to delegate the specific details of digit selection, command execution like write & size of numpad,
+ * and passcode validation to subclasses.
+ *
+ * The subclasses can define specific behaviour how they pull the details from padlock.
+ * {@link ConcretePadlockSolution} -> provides actual implementation to pull details from plan java padlock object .
+ * {@link TcpSocketPadlockSolution} -> a placeholder class to demonstrate if the padlock communicates via a tcp socket.
+ *
+ */
 public abstract class AbstractPadlockSolution {
 
     protected final PadlockImpl padlock;
@@ -14,16 +26,33 @@ public abstract class AbstractPadlockSolution {
         this.padlock = padlock;
     }
 
-    public void solve() {
+    /**
+     * method that initiates the solution and print the solution if found on the std out.
+     *
+     * @return String of passcode
+     * @throws IllegalArgumentException when the padlock is not solvable. Ideally this should not be the case.
+     */
+    public String solve() {
         Integer[] solution = new Integer[padlock.getNumpadSize()];
-        if (fillAndCheck(0, solution, new HashSet<>())) {
-            return;
+        if (backtrack(0, solution, new HashSet<>())) {
+            return Stream.of(solution).map(String::valueOf).collect(Collectors.joining());
         }
 
-        throw new RuntimeException("can't solve");
+        // Should not reach here if the padlock is solvable
+        throw new IllegalArgumentException("Given padlock is not solvable");
     }
 
-    private boolean fillAndCheck(int position, Integer[] solution, Set<Integer> usedDigits) {
+    /**
+     * Recursive method that applies the alogithm
+     * in simple words, it tries to fill all the positions and verifies the passcode.
+     * if correct then return else removes last added digit and try with another combination.
+     *
+     * @param position   The current position (0 to numpadSize-1).
+     * @param solution   solution array.
+     * @param usedDigits Hashset of used digits.
+     * @return true if valid passcode, false otherwise.
+     */
+    private boolean backtrack(int position, Integer[] solution, Set<Integer> usedDigits) {
         if (position == getNumpadSize()) {
             return isPasscodeCorrect();
         }
@@ -34,10 +63,11 @@ public abstract class AbstractPadlockSolution {
                 solution[position] = digit;
                 usedDigits.add(digit);
 
-                if (fillAndCheck(position + 1, solution, usedDigits)) {
+                if (backtrack(position + 1, solution, usedDigits)) {
                     return true;
                 }
 
+                // Backtrack
                 usedDigits.remove(digit);
             }
         }
--- src/main/java/com/cleverthis/interview/padlock/cracker/ConcretePadlockSolution.java
+++ src/main/java/com/cleverthis/interview/padlock/cracker/ConcretePadlockSolution.java
@@ -5,12 +5,20 @@ import com.cleverthis.interview.padlock.PadlockImpl;
 import java.util.ArrayList;
 import java.util.List;
 
-
+/**
+ * Concrete implementation for the plan java padlock object and defines the behaviour for padlock operations like
+ * digit selection, write command, pulling size of numpad, passcode verification.
+ */
 public class ConcretePadlockSolution extends AbstractPadlockSolution {
     public ConcretePadlockSolution(PadlockImpl padlock) {
         super(padlock);
     }
 
+    /**
+     * Provides a list of possible digits for the current padlock.
+     *
+     * @return A list of possible digits from 0 to numpadSize-1.
+     */
     @Override
     protected Iterable<Integer> getDigits() {
         List<Integer> possibleDigits = new ArrayList<>();
@@ -22,7 +30,7 @@ public class ConcretePadlockSolution extends AbstractPadlockSolution {
 
     @Override
     protected void executeWriteInputBuffer(int position, int digit) {
-        padlock.writeInputBuffer(position, digit);
+        padlock.writeInputBuffer(position, digit); // Expensive operation
     }
 
     @Override
